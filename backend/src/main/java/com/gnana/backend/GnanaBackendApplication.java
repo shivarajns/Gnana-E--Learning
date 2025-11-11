b72d5e242215c4c7cd2edfcc5410214941717fb2
Shivaraju NS <shivarajsmurthy@gmail.com>
2025-11-11 14:42:10 +0530

    Initial commit: setup Gnana project structure

---
+++ backend/src/main/java/com/gnana/backend/GnanaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gnana.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GnanaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GnanaBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/gnana/backend/GnanaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gnana.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GnanaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
